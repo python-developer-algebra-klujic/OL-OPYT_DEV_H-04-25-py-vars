a94b23d6cf08dd1fad93b9898ed87de284a90792
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-01 20:48:51 +0200

    Create vars_02.py

---
+++ vars_02.py
@@ -0,0 +1,6 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) za:
+ - Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
+ - Stranice a i b, četverokuta te za površinu tog četverokuta.
+'''
+
