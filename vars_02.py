480c254eb25be39d3de7823d7ca071b36382216a
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-01 21:04:57 +0200

    Update vars_02.py

--- vars_02.py
+++ vars_02.py
@@ -4,3 +4,15 @@ Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) za:
  - Stranice a i b, četverokuta te za površinu tog četverokuta.
 '''
 
+first_name = 'Pero'
+last_name = 'Pero'
+year_of_birth = 1998
+contry_of_birth = 'Hrvatska'
+employment_status = True
+weight = 80.99
+gender = 'male'
+
+
+a = 5
+b = 6
+p = a * b
