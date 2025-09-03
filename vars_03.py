4df7c5e81ac84c92f2f5613d90f7cdd4202ac289
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-03 19:09:19 +0200

    Update vars_03.py

--- vars_03.py
+++ vars_03.py
@@ -3,22 +3,34 @@
 '''
 Potrebno je kreirati variajble u kojima će biti pohranjene proizvoljne dužine trokuta.
 '''
+avg_score = 5
+b = 6
+c = 7
 
 
 '''
 Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o filmu: naziv filma, 
 godina proizvodnje, proječna ocjena, kratki opis
 '''
+movie = 'The Matrix'
+release_year = 1999
+avg_score = 9.99
+summary = 'Lorem ipsum dolor sit ...'
 
 
 '''
 Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o imenu i prezimenu studenta, 
 završnoj ocjeni te podataka je li diplomirao ili nije
 '''
+first_name = 'Pero'
+last_name = 'Peric'
+final_score = 4.98
+is_graduated = True
 
 
 '''
 Kreirati varijablu za čuvanje podataka o studentu: ime i prezime, OIB, email, 
 broj telefona, prosječna ocjena, godina upisa studija, naziv studija, naziv sveučilišta
 '''
-
+# Opcija 1
+student = 'Pero Peric; 12345678901; pero.peric@email.com; ...'
