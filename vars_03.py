227500c2eb1e314d7dc4ba4507b914d87c100757
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-03 18:40:31 +0200

    Create vars_03.py

---
+++ vars_03.py
@@ -0,0 +1,24 @@
+# Ovo je komentar
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjene proizvoljne dužine trokuta.
+'''
+
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o filmu: naziv filma, 
+godina proizvodnje, proječna ocjena, kratki opis
+'''
+
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o imenu i prezimenu studenta, 
+završnoj ocjeni te podataka je li diplomirao ili nije
+'''
+
+
+'''
+Kreirati varijablu za čuvanje podataka o studentu: ime i prezime, OIB, email, 
+broj telefona, prosječna ocjena, godina upisa studija, naziv studija, naziv sveučilišta
+'''
+
